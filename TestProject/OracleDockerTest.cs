63b0542f5f844d05426cf2808fb3880738f18462
Anthony COUDENE <anthony.coudene@technidata-web.com>
2022-10-02 19:20:09 +0200

    Add connectionString Devart format for Oracle

--- TestProject/OracleDockerTest.cs
+++ TestProject/OracleDockerTest.cs
@@ -6,57 +6,83 @@ using Microsoft.EntityFrameworkCore;
 
 namespace TestProject
 {
-  public class OracleDockerTest : IAsyncLifetime
-  {
-    private readonly TestcontainerDatabase _testcontainers = new TestcontainersBuilder<OracleTestcontainer>()
-        .WithDatabase(new OracleTestcontainerConfiguration())
-        .Build();
-
-    [Fact]
-    public void ReadPatients_Ok()
+    public class OracleDockerTest : IAsyncLifetime
     {
-      // Arrange
-      var patients = default(List<Patient>);
-      var builder = new DbContextOptionsBuilder();
-      builder.UseOracle(_testcontainers.ConnectionString);
+        private const string LicenseKey = "";
+        private readonly TestcontainerDatabase _testcontainers = new TestcontainersBuilder<OracleTestcontainer>()
+            .WithDatabase(new OracleTestcontainerConfiguration())
+            .Build();
 
-      // Act
-      using (var context = new AceContext(builder.Options))
-      {
-        context.Database.EnsureCreated();
-        patients = context.Patients?.ToList();
-      }
+        [Fact]
+        public void ReadPatients_Ok()
+        {
+            // Arrange
+            var patients = default(List<Patient>);
+            var builder = new DbContextOptionsBuilder();
 
-      // Assert
-      Assert.NotNull(patients);
-      Assert.True(patients?.All(patient => patient.PatientId > 0));
+            builder.UseOracle(GetConnectionString());
+
+            // Act
+            using (var context = new AceContext(builder.Options))
+            {
+                patients = context.Patients?.ToList();
+            }
+
+            // Assert
+            Assert.NotNull(patients);
+            Assert.True(patients?.All(patient => patient.PatientId > 0));
+        }
+
+        [Fact]
+        public void ReadPrescribedTests_Ok()
+        {
+            // Arrange
+            var prescribedTests = default(List<PrescribedTest>);
+            var builder = new DbContextOptionsBuilder();
+
+            builder.UseOracle(GetConnectionString());
+
+            // Act
+            using (var context = new AceContext(builder.Options))
+            {
+                prescribedTests = context.PrescribedTests?.ToList();
+            }
+
+            // Assert
+            Assert.NotNull(prescribedTests);
+            Assert.True(prescribedTests?.All(prescribedTest => prescribedTest.PrescribedTestId > 0));
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _testcontainers.StartAsync();
+            SeedData();
+        }
+
+        protected void SeedData()
+        {
+            var patients = SeedDataHelper.GeneratePatients();
+
+            var builder = new DbContextOptionsBuilder();
+            builder.UseOracle(GetConnectionString());
+
+            using (var context = new AceContext(builder.Options))
+            {
+                context.Database.EnsureCreated();
+
+                context.Patients!.AddRange(patients.ToArray());
+                context.SaveChanges();
+            }
+        }
+
+        protected string GetConnectionString()
+        {
+            return $"{_testcontainers.ConnectionString};Direct=true;SID=xe;License Key={LicenseKey}";
+        }
+
+        public Task DisposeAsync()
+        {
+            return _testcontainers.DisposeAsync().AsTask();
+        }
     }
-
-    public async Task InitializeAsync()
-    {
-      await _testcontainers.StartAsync();
-      SeedData();
-    }
-
-    protected void SeedData()
-    {
-      var patients = SeedDataHelper.GeneratePatients();
-
-      var builder = new DbContextOptionsBuilder();
-      builder.UseOracle(_testcontainers.ConnectionString);
-
-      using (var context = new AceContext(builder.Options))
-      {
-        context.Database.EnsureCreated();
-
-        context.Patients!.AddRange(patients.ToArray());
-        context.SaveChanges();
-      }
-    }
-
-    public Task DisposeAsync()
-    {
-      return _testcontainers.DisposeAsync().AsTask();
-    }
-  }
 }
